54fabc78fac5d03edf913f753bdaff3a82df5a37
roman <romannsergeev@gmail.com>
2016-10-18 14:49:42 +0600

    Basic scheme of changing DB records values

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:user][:name]
+    @user.update_attributes(name: params[:user][:name])
+  end
+end
