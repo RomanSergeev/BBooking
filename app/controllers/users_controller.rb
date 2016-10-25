2210696591ce352021744cd99a8b80592b38f8a0
roman <romannsergeev@gmail.com>
2016-10-25 17:59:28 +0600

    Add jsonb field publicdata to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     @user.name = params[:user][:name]
-    @user.update_attributes(name: params[:user][:name])
+	  @user.update_attributes(name: params[:user][:name])
+		redirect_to user_path #maybe routes are best
   end
 
 	def edit_profile
 		@user = User.preload(:profile).find(params[:id])
 		render layout: "user"
-		#render "edit_profile" #magic by default
 	end
 
   def edit_services
 	  @user = User.preload(:profile).find(params[:id])
 	  render layout: "user"
-	  #render "edit_profile" #magic by default
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021104216) do
+ActiveRecord::Schema.define(version: 20161024071304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.jsonb    "personaldata"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
