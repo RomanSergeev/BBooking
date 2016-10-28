012db481e4c539fc802dda879c083dec82b19c03
roman <romannsergeev@gmail.com>
2016-10-28 11:45:49 +0600

    Fix form authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+	protect_from_forgery with: :exception
   before_action :authenticate_user!
-  # rescue_from ActionController::InvalidAuthenticityToken do |exception|
-  #   super
-  # end
 
   def after_sign_in_path_for(user)
     user_path(user)
