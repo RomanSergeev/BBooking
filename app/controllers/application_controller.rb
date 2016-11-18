38e04e87e36feb9a5a4997e111fcaefaecfa1075
roman <romannsergeev@gmail.com>
2016-11-18 15:43:37 +0600

    Fix search query; Add user link on search results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,28 @@ class ApplicationController < ActionController::Base
   end
 
   def search
-    params[:q] ||= ''
-    query = "to_tsquery('" + params[:q] + "')"
+    # TODO make some universal query preparator
+    param = params[:q] || ''
+    param.delete!('&')
+    param.delete!('|')
+    param.squeeze!(' ')
+    param.strip!
+    param.gsub!(' ', ' | ')
+    # single-quotes are essential for correct PostgreSQL ts_query
+    query = 'to_tsquery(\'' + param + '\')'
     @records = Service.find_by_sql(
-      "SELECT id, servicedata, ts_headline(textsearchable_index_col, " + query +
-        ") as tooltip FROM services WHERE to_tsvector(textsearchable_index_col) @@ " + query + ";"
+      'SELECT * FROM (' +
+        'SELECT ' +
+          'services.id AS id, ' +
+          'services.user_id, ' +
+          'services.servicedata, ' +
+          'ts_headline(textsearchable_index_col, ' + query + ') AS tooltip, ' +
+          'profiles.personaldata AS userdata, ' +
+          'ts_rank_cd(to_tsvector(textsearchable_index_col), ' + query + ') AS rank ' +
+        'FROM services JOIN profiles ON services.user_id = profiles.user_id ' +
+      ') t ' +
+      'WHERE t.rank > 0 ' +
+      'ORDER BY t.rank desc;'
     )
     render 'search/index', layout: 'search'
   end
